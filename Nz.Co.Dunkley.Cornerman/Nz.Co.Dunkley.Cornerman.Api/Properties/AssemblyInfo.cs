0d3e925b02faa799d5f55dfbeaae9034eddeb073
David Dunkley <daviddunkley@gmail.com>
2015-07-08 16:37:52 +0100

    Added the Backend Web Application Project
    Added Nz.Co.Dunkley.Cornerman.Api project. Currently does not have
    WebApi but I will add that later.

---
+++ Nz.Co.Dunkley.Cornerman/Nz.Co.Dunkley.Cornerman.Api/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Nz.Co.Dunkley.Cornerman.Api")]
+[assembly: AssemblyDescription("Backend WebApp for the Cornerman Project")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Dunkley.Co.Nz")]
+[assembly: AssemblyProduct("Nz.Co.Dunkley.Cornerman.Api")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4a5944f2-a3a2-4909-96a8-f3a1b49d18db")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
